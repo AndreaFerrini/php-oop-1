62f1167e149aad51d8ab834402a3a6bc06dcfe1a
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-06-03 17:35:37 +0200

    inserito header.php + costruttore

--- film.php
+++ film.php
@@ -1,10 +1,26 @@
 <?php
 
 class Film {
+
     public $titolo;
     public $anno;
     public $genere;
     public $durata;
+
+    public function __construct($_titolo, $_anno, $_genere, $_durata){
+        
+        $this->titolo = $_titolo;
+        $this->anno = $_anno;
+        $this->genere = $_genere;
+        $this->durata = $_durata;
+
+    }
+
+    public function get_movie_deails(){
+        return "Film: $this->titolo, ha una durata di: $this-> durata";
+    }
+    
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
 include_once __DIR__ . '/film.php';
+include_once __DIR__ . '/partials/header.php';
+
 
 ?>
